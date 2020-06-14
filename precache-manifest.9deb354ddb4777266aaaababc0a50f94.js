15bbf43a7482dbc791b38c695eda85a1effbedf0
lunar-byte <56989528+lunar-byte@users.noreply.github.com>
2020-06-14 20:32:36 +0500

    Updates

---
+++ precache-manifest.9deb354ddb4777266aaaababc0a50f94.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c689603bdbb46cadc5479f115d461f",
+    "url": "/mainapp/index.html"
+  },
+  {
+    "revision": "a2e003c16d79dee69ebb",
+    "url": "/mainapp/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "981a2903239c69fbcd12",
+    "url": "/mainapp/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "a2e003c16d79dee69ebb",
+    "url": "/mainapp/static/js/2.d38b9f80.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/mainapp/static/js/2.d38b9f80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "981a2903239c69fbcd12",
+    "url": "/mainapp/static/js/main.fc559197.chunk.js"
+  },
+  {
+    "revision": "efa3681264a5e5cea148",
+    "url": "/mainapp/static/js/runtime-main.13b436e7.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/mainapp/static/media/persik.4e1ec840.png"
+  }
+]);
